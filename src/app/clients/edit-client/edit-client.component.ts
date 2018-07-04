e7d5bfca579d19ee213f9b60c90a0548a4fa2114
123samir <123samir@gmail.com>
2018-07-03 22:52:55 -0300

    inclusao de inputs adicionais e populando na modal de resumo

--- src/app/clients/edit-client/edit-client.component.ts
+++ src/app/clients/edit-client/edit-client.component.ts
@@ -23,29 +23,43 @@ export class EditClientComponent implements OnInit {
   confirmDelete = false;
   pageEditActions;
 
-  public readonly newUserActions: Array<ThfPageAction> = [
-    { label: 'Adicionar Cliente', action: this.addClient.bind(this, this.customer), icon: 'thf-icon-plus' },
-    { label: 'Voltar', action: () => this.location.back() }
-  ];
+  public readonly confirmDeleteAction: ThfModalAction = {
+    action: () => this.onConfirmDelete(), label: 'Deletar'
+  };
+
+  public readonly cancelDeleteAction: ThfModalAction = {
+    action: () => this.modalDeleteUser.close(), label: 'Cancelar'
+  };
 
   public readonly editUserActions: Array<ThfPageAction> = [
     { label: 'Salvar Cliente', action: this.updateClient.bind(this, this.customer), icon: 'thf-icon-plus' },
     { label: 'Deletar', action: () => this.modalDeleteUser.open() },
     { label: 'Voltar', action: () => this.location.back() }
   ];
-
-
-  public readonly modalPrimaryAction: ThfModalAction = {
-    action: () => this.returnToHome(() => this.formModal.close()),
-    label: 'Fechar'
-  };
-
+  
   public readonly editUserBreadcrumb: ThfBreadcrumb = {
     items: [
       { label: 'Clientes', link: '/clients' },
       { label: 'Editar Cliente', link: '/clients/edit-client' }
     ]
   };
+
+  public readonly modalPrimaryAction: ThfModalAction = {
+    action: () => this.returnToHome(() => this.formModal.close()),
+    label: 'Fechar'
+  };
+
+  public readonly nationalityOptions: Array<ThfSelectOption> = [
+    { label: 'Coruscant', value: 'coruscant' },
+    { label: 'Death Star', value: 'deathstar' },
+    { label: 'Kamino', value: 'kamino' },
+    { label: 'Naboo', value: 'naboo' }
+  ];
+
+  public readonly newUserActions: Array<ThfPageAction> = [
+    { label: 'Adicionar Cliente', action: this.addClient.bind(this, this.customer), icon: 'thf-icon-plus' },
+    { label: 'Voltar', action: () => this.location.back() }
+  ];
 
   public readonly newUserBreadcrumb: ThfBreadcrumb = {
     items: [
@@ -54,35 +68,20 @@ export class EditClientComponent implements OnInit {
     ]
   };
 
-  readonly statusOptions: Array<ThfSelectOption> = [
+  public readonly personalityOptions: Array<ThfCheckboxGroupOption> = [
+    { value: 'Crafter', label: 'Crafter' },
+    { value: 'Inventor', label: 'Inventor' },
+    { value: 'Protetor', label: 'Protetor' },
+    { value: 'Controlador', label: 'Controlador' },
+    { value: 'Performer', label: 'Performer' },
+    { value: 'Idealista', label: 'Idealista' }
+  ];
+
+  public readonly statusOptions: Array<ThfSelectOption> = [
     { label: 'Rebel', value: 'rebel' },
     { label: 'Tattoine', value: 'tatooine' },
     { label: 'Galactic', value: 'galactic' }
   ];
-
-  readonly nationalityOptions: Array<ThfSelectOption> = [
-    { label: 'Coruscant', value: 'coruscant' },
-    { label: 'Death Star', value: 'deathstar' },
-    { label: 'Kamino', value: 'kamino' },
-    { label: 'Naboo', value: 'naboo' }
-  ];
-
-  public readonly personalityOptions: Array<ThfCheckboxGroupOption> = [
-    { value: '1', label: 'Crafter' },
-    { value: '2', label: 'Inventor' },
-    { value: '3', label: 'Protetor' },
-    { value: '4', label: 'Controlador' },
-    { value: '5', label: 'Performer' },
-    { value: '6', label: 'Idealista' }
-  ];
-
-  public readonly confirmDeleteAction: ThfModalAction = {
-    action: () => this.onConfirmDelete(), label: 'Deletar'
-  };
-
-  public readonly cancelDeleteAction: ThfModalAction = {
-    action: () => this.modalDeleteUser.close(), label: 'Cancelar'
-  };
 
   @ViewChild('modalDeleteUser') modalDeleteUser: ThfModalComponent;
 
@@ -99,48 +98,49 @@ export class EditClientComponent implements OnInit {
   ngOnInit() {
     this.getClient();
   }
-
-  private addClient(customer: Customer) {
+  
+  addClient(customer: Customer) {
     this.clientsService.addClient(customer);
     this.formModal.open();
   }
-
-  private getClient() {
+  
+  deleteClient() {
+    this.clientsService.deleteClient(this.customer.id).subscribe(data => {
+      this.router.navigate(['/clients']);
+      this.thfNotification.success('O usuário foi excluído.');
+    });
+  }
+  
+  getClient() {
     const id = this.route.snapshot.paramMap.get('id');
     const result = this.clientsService.getClient(id);
     if (id && result) {
       this.pageEditActions = true;
       result.subscribe(params => {
         this.customer = params;
+        console.log(this.customer);
       });
     }
-  }
 
-  private returnToHome(param: Function) {
+  }
+  
+  onConfirmDelete() {
+    this.confirmDelete = true;
+    this.modalDeleteUser.close();
+    this.deleteClient();
+  }
+  
+  returnToHome(param: Function) {
     this.clientsService.getClients();
     this.router.navigate(['/clients']);
   }
 
-    private updateClient() {
-      this.route.params.subscribe(params => {
+  updateClient() {
+    this.route.params.subscribe(params => {
       this.clientsService.updateClient(this.customer);
       this.router.navigate(['/clients']);
       this.thfNotification.success('Alteração efetuada com sucesso.');
     });
   }
-
-  private onConfirmDelete() {
-    this.confirmDelete = true;
-    this.modalDeleteUser.close();
-    this.deleteClient();
-  }
-
-  private deleteClient() {
-    this.clientsService.deleteClient(this.customer.id).subscribe(data => {
-      this.router.navigate(['/clients']);
-      this.thfNotification.success('O usuário foi excluído.');
-    });
-  }
-
 
 }
